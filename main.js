980812bb0d0035bcd51738cae2201969c13381d2
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-09-26 17:10:57 +0000

    beer

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+
+function getData() {
+
+    let state = document.getElementById("stateInput").value;
+    let city = document.getElementById("cityInput").value;
+
+
+    let url = "https://api.openbrewerydb.org/breweries"
+    let urlState = "?by_state=" + state
+    let urlCity = "&by_city=" + city
+    fetch(url + urlState + urlCity + "&per_page=50")
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+            let beer = `<h2>Breweries</h2>`
+
+            for (let i = 0; i < data.length; i++) {
+                beer += `<ul><li>${data[i].name}</li></ul>`
+            }
+
+            document.getElementById('Breweries').innerHTML = beer
+
+
+        })
+}
+document.getElementById('button').onclick = function () {
+    getData();
+}
