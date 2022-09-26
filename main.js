395ba5f6c2e5e0cb91d6cc949bda4376bfd897ba
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-09-26 20:58:10 +0000

    broken still- inputs not right

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-let state = document.getElementById("stateInput");
-let city = document.getElementById("cityInput");
+let state = document.getElementById("stateInput").value;
+let city = document.getElementById("cityInput").value;
 
 
 let url = "https://api.openbrewerydb.org/breweries";
-let urlState = "?by_state=" + state.value;
-let urlCity = "&by_city=" + city.value;
+let urlState = "?by_state=" + `${state}`;
+let urlCity = "&by_city=" + `${city}`;
 let button = document.getElementById('button');
 
 function getData() {
@@ -19,8 +19,8 @@ function getData() {
         });
         console.log(urlCity);
         console.log(urlState);
-        console.log(state.value);
-        console.log(city.value);
+        console.log(state);
+        console.log(city);
         console.log(url+urlState+urlCity+'&per_page=50')
 }
 button.addEventListener('click', getData)
