875cce848e2021925512c0e116f04691f8f99ab3
McTimidation <michael.mcgraw@student.awesomeinc.org>
2022-09-26 20:22:31 +0000

    image fix

--- main.js
+++ main.js
@@ -1,31 +1,23 @@
-
+let state = document.getElementById("stateInput").value;
+let city = document.getElementById("cityInput").value;
+let url = "https://api.openbrewerydb.org/breweries"
+let urlState = "?by_state=" + state
+let urlCity = "&by_city=" + city
+let button = document.getElementById('button')
 
 
 function getData() {
 
-    let state = document.getElementById("stateInput").value;
-    let city = document.getElementById("cityInput").value;
-
-
-    let url = "https://api.openbrewerydb.org/breweries"
-    let urlState = "?by_state=" + state
-    let urlCity = "&by_city=" + city
+    
     fetch(url + urlState + urlCity + "&per_page=50")
         .then(response => response.json())
         .then(data => {
             console.log(data);
-
             let beer = `<h2>Breweries</h2>`
-
             for (let i = 0; i < data.length; i++) {
                 beer += `<ul><li>${data[i].name}</li></ul>`
             }
-
             document.getElementById('Breweries').innerHTML = beer
-
-
         })
 }
-document.getElementById('button').onclick = function () {
-    getData();
-}
+button.addEventListener('click', getData)
